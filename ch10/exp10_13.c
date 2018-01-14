40c76b11de4f2d741e23f829d07a4033dd45111e
Tianpei Luo <tluo4@stevens.edu>
2018-01-13 22:49:38 -0800

    weekend

---
+++ ch10/exp10_13.c
@@ -0,0 +1,16 @@
+#include "apue.h"
+
+static void sig_int(int);
+
+int main(void)
+{
+    sigset_t newmask, oldmask, waitmask;
+
+    pr_mask("program start: ");
+
+    if (signal(SIGINT, sig_int) == SIG_ERR)
+        err_sys("signal(SIGINT) error");
+    sigemptyset(&waitmask);
+    sigaddset(&waitmask, SIGUSR1);
+    sigemptyset(&newmask);
+}
