f0aba19723c4f738fd5ae486d2bb4c21469a032b
TeslaSama <tluo4@stevens.edu>
2018-01-15 14:07:06 -0800

    sigsuspend

--- ch10/exp10_13.c
+++ ch10/exp10_13.c
@@ -39,3 +39,31 @@ static void
 sig_int(int signo){
     pr_mask("\nin sig_int: ");
 }
+
+void
+pr_mask(const char *str)
+{
+	sigset_t sigset;
+	int errno_save;
+
+	errno_save = errno;
+	if (sigprocmask(0, NULL, &sigset) < 0) {
+		err_ret("sigprocmask error");
+	} else {
+		printf("%s", str);
+		if (sigismember(&sigset, SIGINT))
+			printf(" SIGINT");
+		if (sigismember(&sigset, SIGQUIT))
+			printf(" SIGQUIT");
+		if (sigismember(&sigset, SIGUSR1))
+			printf(" SIGUSR1");
+		if (sigismember(&sigset, SIGALRM))
+			printf(" SIGALRM");
+
+		/* remaining signals can go here */
+
+		printf("\n");
+	}
+
+	errno = errno_save;
+}
